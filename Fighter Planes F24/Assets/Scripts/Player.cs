a9ed87c61a51dee8dea72714701efc0f52e08134
ChaosDais <73835757+ChaosDais@users.noreply.github.com>
2024-11-02 22:56:22 -0400

    Completed Task 1

--- Fighter Planes F24/Assets/Scripts/Player.cs
+++ Fighter Planes F24/Assets/Scripts/Player.cs
@@ -43,9 +43,14 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
 
-        if (transform.position.y > 8f || transform.position.y <= -8f)
+        if (transform.position.y > 0f)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
+            transform.position = new Vector3(transform.position.x, 0f, 0);
+        }
+
+        if (transform.position.y < -4f)
+        {
+            transform.position = new Vector3(transform.position.x, -4f, 0);
         }
     }
 
